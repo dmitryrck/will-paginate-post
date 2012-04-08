a3d564129adf8cfa15fbd768d72a75b82fef50e3
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-08 14:45:43 -0300

    Started project with a base rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WillPaginatePost::Application.config.secret_token = '91c7fb2d40a2dfc28305bcc4bf99b448801b30444efb0b40b06c2abc2210a1b310e484b6e8d138692b5fb45cf984c575e58a1d948bf21f706c3b46acae38939c'
